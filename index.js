522de89797c5e6baf07d7b71aa66ef88e42a34c2
Riccardo <marchettiriccardo04gmail.com>
2023-12-07 16:25:42 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const createnumber = function () {
+  const numberTabel = document.getElementById("tabellone");
+  for (let i = 0; i < 90; i++) {}
+  const numberDiv = document.createElement("div");
+  const number = document.createElement("h3");
+  number.innerText = i + 1;
+  numberDiv.appendChild(numberTabel);
+};
